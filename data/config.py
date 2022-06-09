0d30d00331e9dcb6bdc5d95bcb1257bc1cfe9642
Aidan Vickars <aidanvickars@gmail.com>
2022-06-09 15:54:34 -0700

    adjusted max iters

--- data/config.py
+++ data/config.py
@@ -675,7 +675,7 @@ yolact_base_config = coco_base_config.copy({
     
     # Training params
     'lr_steps': (280000, 600000, 700000, 750000),
-    'max_iter': 6250,
+    'max_iter': 125000,
     
     # Backbone Settings
     'backbone': resnet101_backbone.copy({
